02cde7792ecd73a65351ddb540486de719619bb0
EdwinKinloch <social@kinloch.me>
2019-06-13 10:02:30 -0500

    Created a function called generateRandomColors that will create an array of random rgb colors and assign it to the colors variable. Also created a randomColor function that will generate the numbers for the red, green, and blue.

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
-let colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)",
-];
+const changedColors = (color) => {
+    for(let i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
+}
 
+function pickColor () {
+    var random = Math.floor(Math.random() * colors.length);
+    return colors[random];
+}
+
+const generateRandomColors = (num) =>{
+    //make an array
+    let arr = []
+    //repeat num times
+    for(let i =0; i < num; i++){
+        //get random color and push into arr
+        arr.push(randomColor())
+    }
+    return arr;
+}
+
+const randomColor = () => {
+    //pick a "red" from 0 - 255
+    let r = Math.floor(Math.random() * 256);
+    //pick a "green" from 0 - 255
+    let g = Math.floor(Math.random() * 256);
+    //pick a "blue" from 0 - 255
+    let b = Math.floor(Math.random() * 256);
+    return "rgb(" + r + ", " + g + ", " + b + ")";
+}
+
+let colors = generateRandomColors(6);
 let squares = document.querySelectorAll(".square");
 var pickColor = pickColor();
 let colorDisplay = document.getElementById('colorDisplay');
@@ -27,13 +51,3 @@ for(let i = 0; i < squares.length; i++){
     });
 }
 
-const changedColors = (color) => {
-    for(let i = 0; i < squares.length; i++){
-        squares[i].style.backgroundColor = color;
-    }
-}
-
-function pickColor () {
-    var random = Math.floor(Math.random() * colors.length);
-    return colors[random];
-}
